01bb2e7424b152855c477d80015cfcd68efe35d4
Ahmad Alghozi Ramadhan <37783343+ghozial@users.noreply.github.com>
2018-03-27 10:12:01 +0700

    Reverse Fibonacci

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -37,7 +37,7 @@ public class Fibonacci {
         
         int sum = 0;
         
-        for(int i = 0; i < size; i++){
+        for(int i = size-1; i >= 0; i--){
             sum += fibonacci[i];
             System.out.print(fibonacci[i] + " ");
         }
